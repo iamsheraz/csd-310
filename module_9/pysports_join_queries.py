ca4a599d55009a5c6ed624b2a0a76aac4e4e7165
Muhammad Tariq <81058550+iamsheraz@users.noreply.github.com>
2021-05-02 20:59:40 -0500

    Update pysports_join_queries.py

--- module_9/pysports_join_queries.py
+++ module_9/pysports_join_queries.py
@@ -2,11 +2,7 @@
 #A9.2
 #https://github.com/iamsheraz/csd-310
 
-import mysql.connector
-from mysql.connector import errorcode
-
-
-
+""" database config object """
 config = {
     "user": "pysports_user",
     "password": "MySQL8IsGreat!",
@@ -22,44 +18,14 @@ try:
     
     cursor = db.cursor()
 
-    cursor.execute("INSERT INTO player(first_name, last_name, team_id) VALUES ('Jon', 'Cena', 1)")
-
-
     cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
 
     players = cursor.fetchall()
 
-
-    print("\n  -- DISPLAYING PLAYERS AFTER INSERT --")
-
-    for i in players:
-        print(f"\n Player ID: {i[0]}\n First Name: {i[1]}\n Last Name: {i[2]}\n Team Name: {i[3]}\n")
-    
-
-    print("\n  -- DISPLAYING PLAYERS AFTER UPDATE --")
-
-    cursor.execute("UPDATE player SET team_id = 2, first_name = 'Aqua', last_name = 'Man' WHERE first_name = 'Jon'")
-
-    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
-
-    players = cursor.fetchall()
+    print("\n  -- DISPLAYING PLAYER RECORDS --")
 
     for i in players:
         print(f"\n Player ID: {i[0]}\n First Name: {i[1]}\n Last Name: {i[2]}\n Team Name: {i[3]}\n")
-
-    print("\n  -- DISPLAYING PLAYERS AFTER DELETE --")
-
-    cursor.execute("DELETE FROM player WHERE first_name = 'Jon'")
-    cursor.execute("DELETE FROM player WHERE first_name = 'Cena'")
-
-    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
-
-    players = cursor.fetchall()
-
-    for i in players:
-        print(f"\n Player ID: {i[0]}\n First Name: {i[1]}\n Last Name: {i[2]}\n Team Name: {i[3]}\n")
-
-    
 
    
 
@@ -68,7 +34,7 @@ try:
 
 
 except mysql.connector.Error as err:
- 
+
 
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("  The supplied username or password are invalid")
@@ -80,5 +46,6 @@ except mysql.connector.Error as err:
         print(err)
 
 finally:
-    
+   
+
     db.close()
